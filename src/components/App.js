26b236f6d990f0b85bebb6c79886bb08d490d47e
Aimarest <amarcosestevez@gmail.com>
2022-05-06 15:54:08 +0200

    new branch dev

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import objectToExport from "../services/LocalStorage";
 import Header from "./Header";
 import Filters from "./Filters";
 import MovieSceneList from "./MovieSceneList";
+import MovieSceneDetail from "./MovieSceneDetail";
 
 function App() {
   const [movieScenes, setMovieScenes] = useState(
@@ -59,6 +60,15 @@ function App() {
         }
       }
     });
+  //Buscar cual es la pelicula de la que quiero mostrar el detalle.
+  //Obtenemos la ruta de la aplicación
+  const { pathname } = useLocation();
+  //Buscamos si coincide con la ruta dinámica
+  const dataPath = matchPath("/movie/:movieIndex", pathname);
+  //Buscando el index de la película
+  const movieIndex = dataPath !== null ? dataPath.params.movieIndex : null;
+  //Buscamos toda la información de la escena.
+  const movieFound = movieScenes.find((movie) => index === movieIndex);
   return (
     <div className="App">
       <Header />
@@ -79,6 +89,10 @@ function App() {
                   <MovieSceneList movies={movieFilters} />
                 </>
               }
+            />
+            <Route
+              path="/movie/:movieIndex"
+              element={<MovieSceneDetail movie={movieFound} />}
             />
           </Routes>
         </section>
--- src/components/MovieSceneDetail.js
+++ src/components/MovieSceneDetail.js
@@ -1,7 +1,19 @@
 import "../styles/MovieSceneDetail.scss";
 
-function MovieSceneDetail() {
-  return <div className="MovieSceneDetail"></div>;
+function MovieSceneDetail(props) {
+  return (
+    <section className="MovieSceneDetail">
+      <img
+        src={props.movieScene.poster}
+        className="MovieScene__card__image"
+        alt={`Poster de la película ${props.movieScene.name}`}
+        title={`Poster de la película ${props.movieScene.name}`}
+      />
+      <h2 className="MovieScene__card__name">{props.movieScene.name}</h2>
+      <p className="MovieScene__card__fullLine">{props.movieScene.fullLine}</p>
+      <p className="MovieScene__card__year">{props.movieScene.year}</p>
+    </section>
+  );
 }
 
 export default MovieSceneDetail;
