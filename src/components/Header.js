f2f52a425e0121546592a3e1e78fc3480ce65286
Aimarest <amarcosestevez@gmail.com>
2022-05-07 17:12:01 +0200

    add styles in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import "../styles/Header.scss";
 function Header() {
   return (
     <div className="Header">
-      <h1>Owen wilson's "wow"</h1>
+      <h1 className="Header__title">Owen wilson's "wow"</h1>
     </div>
   );
 }
